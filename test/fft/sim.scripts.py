dff088988899c87a723df31a7ddd8d4c0302ac70
Morteza Ramezani <morteza.r25@gmail.com>
2017-11-17 12:23:19 -0500

    init sniper repo

---
+++ test/fft/sim.scripts.py
@@ -0,0 +1,5 @@
+import sys
+sys.argv = [ "/home/morteza/Works/sniper/scripts/periodic-stats.py", "1000:2000" ]
+execfile("/home/morteza/Works/sniper/scripts/periodic-stats.py")
+sys.argv = [ "/home/morteza/Works/sniper/scripts/markers.py", "markers" ]
+execfile("/home/morteza/Works/sniper/scripts/markers.py")
